48d792e50a95317eaf54f75893e8f783cb5d0769
Hans Raj <hans63565@gmail.com>
2023-02-14 16:39:48 +0500

    added axios to make fetch request using api

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Provider} from './context'
+
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <div className='container'>
-        <Routes>
-          <Route exact={true} path="/" element={<Index/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <Provider>
+      <Router>
+        <Navbar/>
+        <div className='container'>
+          <Routes>
+            <Route exact={true} path="/" element={<Index/>}/>
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
-
+import {Consumer} from '../../context'
 class Tracks extends Component{
     render(){
         return(
-            <div>
-                <h1>Tracks</h1>
-            </div>
+            <Consumer>
+                {value=>{
+                    console.log(value)
+                    return <h1>Tracks</h1>
+                }}
+            </Consumer>
+
         )
     }
 }
