dea73a62d8c0e3c415169e6828ae537b22ee6770
Hans Raj <hans63565@gmail.com>
2023-02-16 23:05:50 +0500

    added Track component to show each track as a bootstrap card

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Track = (props) => {
+
+    const {track} = props
+
+    return (
+        <div className='col-md-6'>
+            <div className='card mb-4 shadow-sm'>
+                <div className='card-body'>
+                    <h5>{track.artist_name}</h5>
+                    <p className='card-text'>
+                        <strong>Track</strong>: {track.track_name}
+                        <br/>
+                        <strong>Album</strong>: {track.album_name}
+                    </p>
+                    <Link to={`lyrics/track/${track.track_id}`} className="btn btn-dark btn-block">
+                         View Lyrics 
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Track
--- src/context.js
+++ src/context.js
@@ -12,7 +12,7 @@ export class Provider extends Component{
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
             .then(res => {
-                this.setState({track_list:res.data.message.body.track_list})
+                this.setState({ track_list:res.data.message.body.track_list })
             })
             .catch(err=>console.log(err))
     }
