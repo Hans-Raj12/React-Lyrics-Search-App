6d84ab2b08f3a85e39b8c5cb355511dff4fbd51a
Hans Raj <hans63565@gmail.com>
2023-02-19 01:14:42 +0500

    built the search component and using dispatch and reducer in our context, update the track list in the state with the search results

--- src/context.js
+++ src/context.js
@@ -3,10 +3,25 @@ import axios from 'axios'
 
 
 const Context = React.createContext()
+
+const reducer = (state,action) =>{
+    switch(action.type){
+        case 'SEARCH_TRACKS':
+            return {
+                ...state,
+                track_list:action.payload,
+                heading:"Search Results"
+            }
+        default:
+            return state
+    }
+}
+
 export class Provider extends Component{
     state={
         track_list:[],
-        heading:'Top 10 Results'
+        heading:'Top 10 Results',
+        dispatch: action => this.setState(state=>reducer(state,action))
     }
 
     componentDidMount() {
